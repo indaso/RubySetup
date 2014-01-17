7f346b8d36901c9f3274d81e3f8c478bfc4a2492
indaso <indaso5@gmail.com>
2014-01-16 23:17:00 -0500

    First commit for setting up Ruby on git for CIS 196

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppName::Application.config.secret_key_base = '724a9c847b707e93687f2d5653a15e2b1050ccdf66c899de6327369355d4f9dc12ef4d7d10b5c5cd30fe3a94385fb0171c6183eefade55cd38ebd8408b76a70c'
